400d036258bb658f93c04675ac9b83b603cb4bc0
a8a3 <38633041+a8a3@users.noreply.github.com>
2018-05-30 23:10:18 +0300

    replace tabs to spaces

--- etc/data_loader/main.lua
+++ etc/data_loader/main.lua
@@ -6,7 +6,7 @@ gInstruments =
     { class_code = "SPBFUT", sec_code = "SRU8", tf = INTERVAL_M5 },
     { class_code = "SPBFUT", sec_code = "SRU8", tf = INTERVAL_M10},
     { class_code = "SPBFUT", sec_code = "SRU8", tf = INTERVAL_M30},
-    { class_code = "SPBFUT", sec_code = "SRU8", tf = INTERVAL_H1},	
+    { class_code = "SPBFUT", sec_code = "SRU8", tf = INTERVAL_H1},    
 };
 
 -- конфигурация
@@ -37,59 +37,59 @@ function getData( _class_code, _sec_code, _tf )
     
     if ( not candles ) then 
         message( "Can't get data source : " .. (nil == errorDesk and "Unknown Error!" or errorDesk) .. " Skip data load", 3 );
-		return;
+        return;
     end
    
     if ( not candles:SetEmptyCallback() ) then
-		candles:Close();
-		message( "Can't get data from server. Skip data load", 3 );
-		return;
-	end 
+        candles:Close();
+        message( "Can't get data from server. Skip data load", 3 );
+        return;
+    end 
 
-	if ( 0 == candles:Size() ) then
-		message( "Wait server response...", 2 );
-	end
+    if ( 0 == candles:Size() ) then
+        message( "Wait server response...", 2 );
+    end
 
-	local stopWaitTime = os.time() + gConfig.WAIT_TIME;
+    local stopWaitTime = os.time() + gConfig.WAIT_TIME;
 
     while (errorDesk == "" or errorDesk == nil) and (0 == candles:Size()) and (stopWaitTime > os.time())
-		do sleep( 1 );
-	end
-	
+        do sleep( 1 );
+    end
+    
     if ( 0 == candles:Size() ) then 
-		message( "Data source is empty: ".. (nil == errorDesk and "Unknown Error!" or errorDesk) .. " Skip data load" , 3 );
-		return;
-	end
+        message( "Data source is empty: ".. (nil == errorDesk and "Unknown Error!" or errorDesk) .. " Skip data load" , 3 );
+        return;
+    end
 
-	local fileName = gConfig.OUT_FOLDER_NAME .. "\\" .. _class_code .. "_" .. _sec_code .. "_" .. tostring(_tf) .. "_M" .. ".csv";
-	local file = io.open( fileName, 'w' );	
-	
-	if ( not file ) then
-		candles:Close();
-		message( "Can't create file '" .. fileName .. "'" .. " Skip data load", 3 );
-		return;
-	end
-		
-	file:write( "<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE> \n" );
-	
-	for i = 1, candles:Size()-1, 1 do	
-		file:write( candles:T(i).year .. string.format("%02d", candles:T(i).month) .. string.format("%02d", candles:T(i).day) .. "," .. string.format("%02d", candles:T(i).hour) .. string.format("%02d", candles:T(i).min).. string.format("%02d", candles:T(i).sec) .. "," .. 
-					candles:O(i) .. "," .. 
-					candles:H(i) .. "," .. 
-					candles:L(i) .. "," .. 
-					candles:C(i) .. "\n");
-	end
-				
-	file:close();
+    local fileName = gConfig.OUT_FOLDER_NAME .. "\\" .. _class_code .. "_" .. _sec_code .. "_" .. tostring(_tf) .. "_M" .. ".csv";
+    local file = io.open( fileName, 'w' );    
+    
+    if ( not file ) then
+        candles:Close();
+        message( "Can't create file '" .. fileName .. "'" .. " Skip data load", 3 );
+        return;
+    end
+        
+    file:write( "<DATE>,<TIME>,<OPEN>,<HIGH>,<LOW>,<CLOSE> \n" );
+    
+    for i = 1, candles:Size()-1, 1 do    
+        file:write( candles:T(i).year .. string.format("%02d", candles:T(i).month) .. string.format("%02d", candles:T(i).day) .. "," .. string.format("%02d", candles:T(i).hour) .. string.format("%02d", candles:T(i).min).. string.format("%02d", candles:T(i).sec) .. "," .. 
+                    candles:O(i) .. "," .. 
+                    candles:H(i) .. "," .. 
+                    candles:L(i) .. "," .. 
+                    candles:C(i) .. "\n");
+    end
+                
+    file:close();
     candles:Close();
-	message( "Data saved to '" .. fileName .. "'", 1 );
+    message( "Data saved to '" .. fileName .. "'", 1 );
 end
 
 --
 function main()
-	for _, _item in ipairs( gConfig.INSTRUMENTS ) do
-		getData( _item.class_code, _item.sec_code, _item.tf );
-	end
-	message( "Done." );
+    for _, _item in ipairs( gConfig.INSTRUMENTS ) do
+        getData( _item.class_code, _item.sec_code, _item.tf );
+    end
+    message( "Done." );
 end
 
